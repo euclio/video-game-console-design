53f1275105dee13ab50c7e95bb0cfd0145dea9c2
Andy Russell <arussell123@gmail.com>
2015-04-26 16:45:00 -0700

    show fullscreen window

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,23 +1,27 @@
 #include <opencv2/highgui/highgui.hpp>
 
+#include <iostream>
+
 #include "hand.hpp"
 #include "handDetection.hpp"
 #include "median.hpp"
 #include "silhouette.hpp"
 
 int main() {
-    const auto windowName = "median";
+    const auto mainWindowName = "display";
     cv::VideoCapture webcam(0);
     cv::VideoWriter output;
 
-    cv::namedWindow("median");
+    cv::namedWindow(mainWindowName, CV_WINDOW_NORMAL);
+    cv::setWindowProperty(mainWindowName,
+                           CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
     cv::namedWindow("silhouette");
 
     cv::Mat frame, silhouette;
     webcam >> frame;
 
     auto regionsOfInterest = getRegionsOfInterest(frame);
-    auto colorSamples = findAverageColorOfHand(webcam, windowName);
+    auto colorSamples = findAverageColorOfHand(webcam, mainWindowName);
     do {
         webcam >> frame;
 
@@ -30,10 +34,9 @@ int main() {
             hand->drawContours(frame);
         }
 
-        cv::imshow("median", frame);
         cv::imshow("silhouette", silhouette);
-
-    } while(cv::waitKey(30) != char('q'));
+        cv::imshow(mainWindowName, frame);
+    } while((cv::waitKey(30) & 0xFF) != 'q');
 
     cv::destroyAllWindows();
     output.release();
